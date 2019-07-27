8545ee423e1968c43ec3ab58d2e7355e667cd0a3
Einar Strandberg <sd.estrandberg@mtsu.edu>
2019-07-26 21:37:05 -0700

    Have LTT' working without U.Bind

--- src/Core/LTT.hs
+++ src/Core/LTT.hs
@@ -2,6 +2,8 @@
   PatternSynonyms, DeriveAnyClass, DataKinds, TypeFamilies,
   FunctionalDependencies, StandaloneDeriving, TypeFamilyDependencies,
   QuasiQuotes #-}
+{-@ LIQUID "--short-names" @-}
+{-@ LIQUID "--exact-data-cons" @-}
 module Core.LTT (
   Var, Decl(..), Binder(..),
   LTT(.., Pi, Lam), viewPi, viewLam
@@ -16,13 +18,14 @@ import Data.Typeable (Typeable)
 -- Library imports
 import qualified Unbound.Generics.LocallyNameless as U
 import Unbound.Generics.LocallyNameless (Embed(..))
-import Unbound.Generics.LocallyNameless.Unsafe as U
-import Unbound.Generics.LocallyNameless.Bind (Bind(B))
+import qualified Unbound.Generics.LocallyNameless.Unsafe as U
+import Unbound.Generics.LocallyNameless.Bind
 
 -- | Variable name representing an LTT term.
 type Var = U.Name LTT
 
-{-@ data Binder [binderLen]
+{-@ autosize Binder @-}
+{-@ data Binder 
   = BPi { piTy :: (Embed LTT) }
   | BLam
   | Hole
@@ -34,64 +37,60 @@ data Binder
   | Guess (Embed LTT)
   deriving (Show, Generic, Typeable, U.Alpha)
 
-{-@ measure binderLen @-}
-binderLen :: Binder -> Int
-binderLen (BPi (Embed ty)) = 1 + lttLen ty --embedLen ety
-binderLen BLam = 0
-binderLen Hole = 0
-binderLen (Guess (Embed ty)) = 1 + lttLen ty
-{-@ invariant {v: Binder | binderLen v >= 0} @-}
-
 type LttBinding = U.Bind Var LTT
-
-{-@ measure isBindingDev @-}
-isBindingDev :: LttBinding -> Bool
-isBindingDev (B _ t) = isLttDev t
-
-termOfBinding :: LttBinding -> LTT
-termOfBinding (B _ t) = t
-
-  {-@ measure bindingLen :: b:LttBinding -> {v:Int | v = lttLen (termOfBinding
- t) @-}
-bindingLen :: LttBinding -> Int
-bindingLen (B _ t) = lttLen t
-{-@ invariant {v: LttBinding | bindingLen v >= 0} @-}
 
 -- | The core dependent calculus. Based on Idris' TT.
 
-{-@ data LTT [lttLen]
+{-@ autosize LTT @-}
+{-@ data LTT
   = Var { var :: Var }
-  | Universe { universeLevel :: Int }
+  | Universe { universeLevel :: Nat }
   | Bind { binder :: Binder, binding :: U.Bind Var LTT }
   | App { appR :: LTT, appL :: LTT } @-}
 data LTT
   = Var Var
   | Universe Int
-  | Bind Binder (U.Bind Var LTT)
+  | Bind Binder LttBinding
   | App LTT LTT
   deriving (Show, Generic, Typeable, U.Alpha)
 
-{-@ measure lttLen @-}
-lttLen :: LTT -> Int
-lttLen (Var _) = 0
-lttLen (Universe _) = 0
-lttLen (Bind binder binding) = 1 + binderLen binder + bindingLen binding
-lttLen (App t1 t2) = 1 + lttLen t1 + lttLen t2
+type Var' = U.Name LTT'
+{-@ autosize LTT' @-}
+{-@ data LTT'
+  = Var' { var' :: Var' }
+  | Universe' { universeLevel' :: Nat }
+  | Pi' { piTy' :: LTT', piVar' :: Var', piBody' :: LTT' }
+  | Lam' { lamVar' :: Var', lamBody' :: LTT' }
+  | Hole' { holeVar' :: Var', holeBody' :: LTT' }
+  | Guess' { guessTy' :: LTT', guessVar' :: Var', guessBody' :: LTT' }
+  | App' { appR' :: LTT', appL' :: LTT' } @-}
+data LTT'
+  = Var' Var
+  | Universe' Int
+  | Pi' LTT' Var' LTT'
+  | Lam' Var' LTT'
+  | Hole' Var' LTT'
+  | Guess' LTT' Var' LTT'
+  | App' LTT' LTT'
+  deriving (Show, Generic, Typeable, U.Alpha)
 
-{-@ invariant {v: LTT | lttLen v >= 0} @-}
+{- @ measure lttLen @-}
+{- @ lttLen :: LTT' -> Nat  @-}
+  {- lttLen :: LTT' -> Int
+lttLen (Var' _) = 0
+lttLen (Universe' _) = 0
+lttLen (Bind' binder _ body) = 1 + max' (binderLen binder) (lttLen body)
+lttLen (App' t1 t2) = 1 + max' (lttLen t1) (lttLen t2) -}
 
-{-@ measure isBinderDev @-}
-isBinderDev :: Binder -> Bool
-isBinderDev (BPi (Embed ty)) = isLttDev ty --isLttDev tyA && isLttDev tyB
-isBinderDev BLam = False -- isLttDev body
-isBinderDev _ = True
-
-{-@ measure isLttDev @-}
-isLttDev :: LTT -> Bool
-isLttDev (Var _) = False
-isLttDev (Universe _) = False
-isLttDev (App _ _) = False
-isLttDev (Bind binder binding) = isBinderDev binder && isBindingDev binding
+{-@ measure isLttDev' @-}
+isLttDev' :: LTT' -> Bool
+isLttDev' (Var' _) = False
+isLttDev' (Universe' _) = False
+isLttDev' (App' _ _) = False
+isLttDev' (Pi' tyA _ tyB) = isLttDev' tyA || isLttDev' tyB
+isLttDev' (Lam' _ body) = isLttDev' body
+isLttDev' (Hole' _ body) = isLttDev' body 
+isLttDev' (Guess' tyA _ tyB) = isLttDev' tyA || isLttDev' tyB
 
 {-@ type LttDev = { v:LTT | isLttDev v } @-}
 {-@ type LttCore = { v:LTT | not (isLttDev v) } @-}
